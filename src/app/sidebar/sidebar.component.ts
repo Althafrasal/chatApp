f668ca6601983a7fb6a9ce1821de6aa3ecd2721c
ALTHAF RASAL <althafrasal.ar@gmail.com>
2023-01-16 00:14:15 +0530

    initial commit

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  @Output() conversationClicked: EventEmitter<any> = new EventEmitter();
+  searchText: string;
+  conversations = [
+    {
+      name: 'Rasal',
+      time: '8:21',
+      latestMessage: 'Hi there!!',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello ', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Althaf',
+      time: '8:21',
+      latestMessage: 'Hi there!!',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello ', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    }
+
+  ];
+
+  get filteredConversations() {
+    return this.conversations.filter((conversation) => {
+      return (
+        conversation.name
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase()) ||
+        conversation.latestMessage
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase())
+      );
+    });
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
